3fd614359a75160dca9b383aaf4d5f6a4060f6bc
mohsenpashna <pashna@gmail.com>
2018-02-21 17:09:52 +1300

    on going solution for missing Integer

--- CodilityLessions.cpp
+++ CodilityLessions.cpp
@@ -37,25 +37,51 @@ int PermCheck() {
 
 int MissingInteger(){
 //    int  result =1;
-    vector<int> A = {1, 3, 6, 4, 1, 2};
-//    vector<int> A = {1, 2, 3};
+//    vector<int> A = {1, 3, 6, 4, 1, 2};
+    vector<int> A = {1, 2, 3};
 //    vector<int> A = {-1, -3};
     int sizeOfA = A.size();
-    int minA = 1;
+
+
+    vector<int> B (sizeOfA, 0);
+
+    for(int i=0;i<sizeOfA;i++){
+        if(A[i]<0) continue;
+        B[A[i]] = 1;
+        cout << B[i]<< endl;
+
+    }
+    bool allTrue = true;
+    for(int i=0;i<sizeOfA;i++){
+//        if(A[i]<0) continue;
+        cout << allTrue << B[i]<< endl;
+        allTrue = allTrue & !B[i] == 0;
+        cout << allTrue << endl;
+        if(B[i] == 0) return i;
+    }
+    if(allTrue) return sizeOfA+1;
+
+    return  1;
+
+//    int minA = 1;
 //    for(vector<int>::iterator it=A.begin(); it !=A.end(); it++){
 //        maxA = ((maxA>*it) ? maxA : *it);
 //    }
 //    cout << "min of A: " << minA<< endl;
-    sort(A.begin(), A.end());
+//    sort(A.begin(), A.end());
 
 //    for(vector<int>::iterator it=A.begin(); it !=A.end(); it++){
 
-    if((A[sizeOfA-1])<0){
-        return  1;
-    }
-    int cntr =1;
-    for(int i=0;i<sizeOfA; i++){
-//    if ()
+//    if((A[sizeOfA-1])<0){
+//        return  1;
+//    }
+//    int cntr =1;
+//    for(int i=1;i<sizeOfA; i++){
+//    if (A[i]-A[i-1]>1){
+//        cout << i << ": Ai is " << A[i] << endl;
+//        return A[i-1]+1;
+//    }
+
 //        cout << "i is is " << i << endl;
 //        cout << "A[i] is is " << A[i] << endl;
 
@@ -66,7 +92,7 @@ int MissingInteger(){
 //        }
 //        cout << "min of A: " << minA << endl;
 //        cout << "A[i]: " << endl;
-    }
+//    }
 
-    return sizeOfA+1;
+//    return sizeOfA+1;
 }
